bb8854a175fb96098d65d3fc504790ed915ffcab
Mahmoud Ashraf <66477803+m-Ash1@users.noreply.github.com>
2023-07-04 18:17:39 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element"
+  }
+];
+
+function FlashCards() {
+  const [selectedID, setSelectedID] = useState(null);
+  function handleSelect(id){
+    if(id==selectedID){setSelectedID(null)}
+    else{setSelectedID(id)}
+    
+  }
+
+  return <div className="flashcards">
+    {questions.map(question => <div onClick={()=>handleSelect(question.id)} className={selectedID === question.id ? "selected" : ""} key={question.id}>
+      {question.id === selectedID ? question.answer : question.question}
+    </div>)}
+  </div>
+}
